14a834b60c4c8a0eeadcda575eef6de73ad19168
BEATRICE <Ne0N__@outlook.com>
2018-09-19 11:15:00 +0300

    dev(parser): Add images

--- Servise/Parser/Deal.py
+++ Servise/Parser/Deal.py
@@ -1,30 +1,31 @@
-from .Engine import Engine
 from bs4 import BeautifulSoup
 import csv
 
 class Deal(object):
     def Parser(html):
+        try:
+            soup = BeautifulSoup(html,"html.parser")
+            table = soup.find('div',class_="x-catalog-gallery__list")
+        except:
+            return "Not found"
 
-        soup = BeautifulSoup(html,"html.parser")
-        table = soup.find('div',class_="x-catalog-gallery__list")
-
-        listObjects = []
-        
+        listObjects = []        
         for elements in table.find_all('div')[::]:
             try:
                 name = elements.find('a',class_="x-gallery-tile__name").text
                 price = str(elements.find('div',class_="x-gallery-tile__price").text).strip()
-                price = price.replace(" руб.","")
-                float(price)
                 link = elements.find('div',class_="x-gallery-tile__extra").find('a').get('href')
-                
+                urlImage = elements.find('img',class_="x-image-holder__img").get('src')
+                price = price.replace(" руб.","")
+                float(price)               
                 if link == None:
                     link = "Private seller or out of stock"
 
                 listObjects.append(
                 {'name':name,
                  'price':price,
-                 'link':link,})                              
+                 'link':link,
+                 'image':urlImage})                              
                 
             except:
                 pass
